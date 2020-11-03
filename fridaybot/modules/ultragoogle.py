8e201598c867f4122a6bb98d7c627f1ba01afe24
suraj3440X <73871588+suraj3440X@users.noreply.github.com>
2020-11-03 12:25:41 +0530

    Update ultragoogle.py

--- fridaybot/modules/ultragoogle.py
+++ fridaybot/modules/ultragoogle.py
@@ -150,10 +150,18 @@ async def _(event):
         img_size = img_size_div.find_all("div")
         end = datetime.now()
         ms = (end - start).seconds
-        OUTPUT_STR = """{img_size}
-**Possible Related Search**: <a href="{prs_url}">{prs_text}</a>
-
-More Info: Open this <a href="{the_location}">Link</a> in {ms} seconds""".format(
+        OUTPUT_STR = """/protecc {prs_text} """.format(
             **locals()
-        )
+
+
+
+
+            
+
+
+
+
+       
+        
+   
     await event.edit(OUTPUT_STR, parse_mode="HTML", link_preview=False)
